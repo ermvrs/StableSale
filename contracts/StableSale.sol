2295d632b34c1b4f7341f757b11fb91a42479e77
ermvrs <ermvrs@live.com>
2022-05-23 16:30:13 +0300

    first commit

---
+++ contracts/StableSale.sol
@@ -0,0 +1,86 @@
+pragma solidity >=0.8.10;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract LockedSale is Ownable {
+
+    uint256 public minAmount = 0.01 ether;
+    bool public saleActive = true;
+    uint256 public price = 0.55 ether; // 1 RV2 = 0.55 BUSD
+
+
+    address public dev;
+    IERC20 public payToken; // busd
+    IERC20 public recvToken; // rv2
+
+
+    constructor(IERC20 _payToken, IERC20 _recvToken, address _dev) {
+        payToken = _payToken;
+        recvToken = _recvToken;
+        dev = _dev;
+    }
+
+    function swapToken(uint256 _amount) public onlySaleActive {
+        require(_amount >= minAmount, "amount too low");
+        uint256 busdBalance = getContractBalance();
+        uint256 amountToPay = (_amount * price) / 1 ether;
+
+        require(busdBalance >= amountToPay, "contract balance low");
+
+        uint256 netAmount = netTransfer(payToken, msg.sender, _amount);
+
+        uint256 netAmountToPay = (netAmount * price) / 1 ether;
+
+        recvToken.transfer(msg.sender, netAmountToPay);
+
+        emit Swap(msg.sender, netAmount, netAmountToPay);
+    }
+
+    // internal functions
+
+    function netTransfer(IERC20 _token, address _from, uint256 _amount) internal returns(uint256) {
+        uint256 before = _token.balanceOf(address(this));
+        _token.transferFrom(_from, address(this), _amount);
+        return _token.balanceOf(address(this)) - before;
+    }
+
+    // view functions
+
+    function getContractBalance() public view returns(uint256) {
+        return payToken.balanceOf(address(this));
+    }
+
+    // owner functions
+
+    function toggleSaleStatus() public onlyOwner {
+        saleActive = !saleActive;
+    }
+
+    function setMinAmount(uint256 _amount) public onlyOwner {
+        minAmount = _amount;
+
+        emit MinAmountChanged(_amount);
+    }
+
+    function setReceiverAddress(address _recv) public onlyOwner {
+        dev = _recv;
+
+        emit ReceiverAddressChanged(_recv);
+    }
+
+    function withdrawTokens(IERC20 _token, uint256 _amount) public onlyOwner {
+        _token.transfer(msg.sender, _amount);
+    }
+
+    // modifiers
+
+    modifier onlySaleActive {
+        require(saleActive, "Sale is not active.");
+        _;
+    }
+
+    event MinAmountChanged(uint256 amount);
+    event ReceiverAddressChanged(address recv);
+    event Swap(address indexed user, uint256 paid, uint256 sent);
+}
